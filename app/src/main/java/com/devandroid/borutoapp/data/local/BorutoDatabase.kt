84e9a5afd368a31190340a22ca4753acd38d1fa2
Rafael <rafaelteicas@gmail.com>
2023-12-31 14:18:37 -0300

    feat: create database converter

--- app/src/main/java/com/devandroid/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/devandroid/borutoapp/data/local/BorutoDatabase.kt
@@ -1,12 +1,14 @@
 package com.devandroid.borutoapp.data.local
 
 import androidx.room.Database
+import androidx.room.TypeConverters
 import com.devandroid.borutoapp.data.local.dao.HeroDao
 import com.devandroid.borutoapp.data.local.dao.HeroRemoteKeyDao
 import com.devandroid.borutoapp.domain.model.Hero
 import com.devandroid.borutoapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase {
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
