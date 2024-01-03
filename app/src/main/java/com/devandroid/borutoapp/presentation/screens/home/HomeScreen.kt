598c8d24745888a05bf9d6b269c18b135edf4bd0
Rafael <rafaelteicas@gmail.com>
2024-01-03 12:09:19 -0300

    feat: create HomeScreen

---
+++ app/src/main/java/com/devandroid/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -0,0 +1,17 @@
+package com.devandroid.borutoapp.presentation.screens.home
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeScreen() {
+    Scaffold (
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }
+    ) {}
+}
